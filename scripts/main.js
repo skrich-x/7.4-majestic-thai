4787595f27c091629738a43de93fd261c7911533
Kyle Richardson <s.k.richardson7@gmail.com>
2015-07-05 22:40:02 -0400

    Moving to Router to implement Admin and Cart

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,28 @@
 import AjaxConfig from './ajax-config';
-
-import MenuItem from './views/itemListView';
-import {Item, ItemListCollection} from './models/itemList';
-
-import Order from './models/order';
-import OrderView from './views/orderView';
-
-import {Categories} from './views/foodCategoryView';
+import router from 'router';
 
 (function(){
   'use strict';
 
   $(document).ready(function(){
+    Backbone.history.start();
 
-    var itemList = new ItemListCollection();
-    console.log('itemList', itemList);
 
-    var order = new Order();
-    console.log('order',order);
-
-    $('#orderTemplate').html(new OrderView({
-      collection: order
-    }).el);
-
-    itemList.fetch().then(function(){
-      var menuItem = new MenuItem({
-        order: order,
-        collection:itemList
-      });
-      $('#menuItemTemplate').html(menuItem.el);
-    //   console.error.bind(console));
-    });
+    // var order = new Order();
+    // console.log('order',order);
+    //
+    // $('#orderTemplate').html(new OrderView({
+    //   collection: order
+    // }).el);
+    //
+    // itemList.fetch().then(function(){
+    //   var menuItem = new MenuItem({
+    //     order: order,
+    //     collection:itemList
+    //   });
+    //   $('#menuItemTemplate').html(menuItem.el);
+    // //   console.error.bind(console));
+    // });
 
   });
   })();
--- scripts/router.js
+++ scripts/router.js
@@ -6,39 +6,60 @@ import {Item, ItemListCollection} from './models/itemList';
 import Order from './models/order';
 import OrderView from './views/orderView';
 
-import {FoodCategoryView} from './views/foodCategoryView';
+import {Categories} from './views/foodCategoryView';
 
 var Router = Backbone.Router.extend({
 
     routes: {
       '': 'index',
-      'administrator': 'administrator',
-      'contact': 'user'
+      // 'administrator': 'administrator',
+      // 'contact': 'user'
     },
 
     initialize: function(){
       this.order = new Order();
-      this.users = new UserCollection();
-      this.menu = new ItemListCollection();
+      // this.users = new UserCollection();
+      this.item = new Item();
+      this.itemList = new ItemListCollection();
+      console.log('itemList');
+
+      //
+      // var order = new Order();
+      // console.log('order',order);
+      //
+      // $('#orderTemplate').html(new OrderView({
+      //   collection: order
+      // }).el);
+      //
+      this.itemList.fetch().then(function(){
+        var menuItem = new MenuItem({collection: this.itemList});
+          // order: order,
+          // collection:itemList
+        $('#menuItemTemplate').html(menuItem.el);
+      }.bind(this));
 
     },
 
     index: function(){
-      this.menu.fetch().then(function (data){
-        this.menuView = new FoodCategoryView({collection: this.menu,
-
-        $('.debugger')})
-      })
 
     },
 
-    administrator: function(){
+    // index: function(){
+    //   this.menu.fetch().then(function (data){
+    //     this.menuView = new FoodCategoryView({collection:
+    //           this.menu,
+    //           order: this.order});
+    //     $('')
+    //
+    // },
 
-    },
-
-    user: function(){
-
-    }
+    // administrator: function(){
+    //
+    // },
+    //
+    // user: function(){
+    //
+    // }
 });
 
 var router = new Router();
